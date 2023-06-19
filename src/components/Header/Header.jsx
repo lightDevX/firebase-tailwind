a14baec5b917dad7683cfb5749f19f5ac73e0c2f
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-06-19 13:48:01 +0600

    LogOut implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider/AuthProvider';
+
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <>
             <div className="navbar bg-base-300">
                 <Link to='/' className="normal-case text-xl px-3">Firebase-Tailwind</Link>
                 <Link to='/' className='norma-case text-xl px-3'>Home</Link>
                 <Link to='/login' className='norma-case text-xl px-3'>Login</Link>
+                {
+                    user ? <>
+                        <span>{user.email}</span>
+                        <button onClick={handleLogOut} className='btn btn-info'>Sign Out</button>
+                    </> : <Link to='/login'>Login</Link>
+                }
             </div>
         </>
     );
