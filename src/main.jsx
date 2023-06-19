1bd92b5ba5e3e9b5fb91c8b43dbf580f11159970
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-06-19 11:38:40 +0600

    Login and Register implemention

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,6 +10,7 @@ import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
 import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login.jsx';
 import Register from './pages/Register/Register.jsx';
+import AuthProvider from './providers/AuthProvider/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -36,6 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProvider/AuthProvider';
 
 const Home = () => {
+
+    const user = useContext(AuthContext);
+
     return (
         <div>
-            
+            <h3>{user && <span>{user.displayName}</span>}</h3>
         </div>
     );
 };
